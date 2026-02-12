4f633226bcccf7c1857d727c28c98743d398143d
Nebil-Abdulfetah <159676747+Nebil-Abdulfetah@users.noreply.github.com>
2026-02-12 19:45:22 +0300

    Add loading spinner feature

---
+++ src/components/BookCard.jsx
@@ -0,0 +1,30 @@
+
+function BookCard({ books }) {
+  const getCover = (book) =>
+    book.cover_i
+      ? `https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`
+      : "/placeholder.jpg";
+  return (
+    books.map((book) => {
+      return (
+        <div key={book.cover_i} className="book-card">
+          <div className="book-img-container">
+            <img
+              className="book-img"
+              src={getCover(book)}
+              alt="Book Cover Page"
+            />
+          </div>
+          <div className="book-details">
+            <p className="book-title">{book.title}</p>
+            <p className="book-author">{book.author_name}</p>
+            <p className="book-date">Date: {book.first_publish_year}</p>
+            <button className="add-to-read">Add To Read</button>
+          </div>
+        </div>
+      );
+    })
+  );
+}
+
+export default BookCard;
--- src/pages/BooksGrid.jsx
+++ src/pages/BooksGrid.jsx
@@ -1,37 +1,20 @@
+import BookCard from "../components/BookCard";
+import loadingSpinner from "../assets/loading-spinner.gif"
 import "./BooksGrid.css";
 
 function BooksGrid({ books }) {
-  const getCover = (book) =>
-    book.cover_i
-      ? `https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`
-      : "/placeholder.jpg";
-  return (
-    <div className="container">
+   return ( 
+    books.length > 0 ? (
+      <div className="container">
       <div className="books-container">
-        {console.log(books)}
-        {books.length > 0 &&
-          books.map((book) => {
-            return (
-              <div key={book.cover_i} className="book-card">
-                <div className="book-img-container">
-                  <img
-                    className="book-img"
-                    src={getCover(book)}
-                    alt="Book Cover Page"
-                  />
-                </div>
-                <div className="book-details">
-                  <p className="book-title">{book.title}</p>
-                  <p className="book-author">{book.author_name}</p>
-                  <p className="book-date">Date: {book.first_publish_year}</p>
-                  <button className="add-to-read">Add To Read</button>
-                </div>
-              </div>
-            );
-          })}
+        {/* {console.log(books)} */}
+        <BookCard books={books}/>
       </div>
     </div>
-  );
-}
+    )
+    : <div className="container loading-container"><img src={loadingSpinner} className="loading"/></div>
+  )
+} 
+
 
 export default BooksGrid;
